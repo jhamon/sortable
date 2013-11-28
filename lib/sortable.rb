9f2db970561f1b266657e2136514da10fe1ec100
Jen Hamon <jhamon@gmail.com>
2013-11-28 14:55:31 -0800

    Add #mergesort

---
+++ lib/sortable.rb
@@ -0,0 +1,23 @@
+module Sortable
+  def mergesort(arr=nil)
+    def merge(left, right)
+      merged = []
+      until left.empty? or right.empty?
+        if left.first < right.first
+          merged << left.shift
+        else
+          merged << right.shift
+        end
+      end
+      merged + left + right
+    end
+
+    arr = self.dup if arr.nil?
+    return arr if arr.length < 2
+
+    middle = arr.length / 2
+    left = arr.take(middle)
+    right = arr.drop(middle)
+    merge(mergesort(left), mergesort(right))
+  end
+end
