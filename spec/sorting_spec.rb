7d050d318ce559d4a40d61e3e220ab9b069f4129
Jen Hamon <jhamon@gmail.com>
2013-11-29 13:45:41 -0800

    Added a couple more specs.

--- spec/sorting_spec.rb
+++ spec/sorting_spec.rb
@@ -6,10 +6,13 @@ class Array
 end
 
 describe "Sorting algorithms" do
-  let(:shuffled_array) { [5, 2, 0, 1, 3] }
+
   let(:empty_array) { [] }
+  let(:shuffled_array) { [5, 2, 0, 1, 3, 17, 15, 11] }
+  let(:another_shuffled_array) { [4, 2, 7, 9, 1, 10, 2] }
   let(:array_with_negative_numbers) { [-10, 4, -2, 5] }
   let(:array_with_duplicates) { [2, 4, 2, 2, 4, 0, -1] }
+  let(:sorted_array) { [-2, 0, 2, 4, 6, 8, 10] }
   let(:reversed_array) { [0, 1, 2, 3, 4, 5].reverse }
 
   Sortable.instance_methods(false).each do |sort_method|
@@ -27,8 +30,16 @@ describe "Sorting algorithms" do
         expect(empty_array.send(sort_method)).to eq([])
       end
 
+      it "handles a sorted array" do
+        expect(sorted_array.send(sort_method)).to eq(sorted_array)
+      end
+
       it "handles a shuffled array" do
         expect(shuffled_array.send(sort_method)).to eq(shuffled_array.sort)
+      end
+
+      it "handles another shuffled array" do
+        expect(another_shuffled_array.send(sort_method)).to eq(another_shuffled_array.sort)
       end
 
       it "handles an array with negative numbers" do
