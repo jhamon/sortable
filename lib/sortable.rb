04d127c97e69f8c1c869cd3e5602fbc93efec455
Jen Hamon <jhamon@gmail.com>
2013-11-29 08:42:38 -0800

    Add #bubblesort

--- lib/sortable.rb
+++ lib/sortable.rb
@@ -1,14 +1,5 @@
 module Sortable
   def mergesort(arr=nil)
-    def merge(left, right)
-      merged = []
-      until left.empty? or right.empty?
-        merged <<
-         ((left.first < right.first) ? left.shift : right.shift)
-      end
-      merged + left + right
-    end
-
     arr = self.dup if arr.nil?
     return arr if arr.length < 2
 
@@ -18,7 +9,35 @@ module Sortable
     merge(mergesort(left), mergesort(right))
   end
 
-  def mergesort!
-    replace(mergesort)
+  def bubblesort
+    arr = self.dup
+    swap_made = true
+    last_index = length - 1
+    until swap_made == false
+      swap_made = false
+
+      each_index do |idx|
+        next if idx == last_index
+        if arr[idx] > arr[idx+1]
+          arr[idx], arr[idx+1] = arr[idx+1], arr[idx]
+          swap_made = true
+        end
+      end
+
+    end
+
+    arr
   end
+
+  private
+    def merge(left, right)
+      merged = []
+
+      until left.empty? or right.empty?
+        merged <<
+         ((left.first < right.first) ? left.shift : right.shift)
+      end
+
+      merged + left + right
+    end
 end
